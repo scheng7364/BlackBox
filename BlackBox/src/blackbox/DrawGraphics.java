b491162d1c4d898b20ab41b95cbe5faf4ac0f847
YuyuXiao <YuyuXiao@users.noreply.github.com>
2015-08-15 23:48:13 -0700

    Update DrawGraphics.java
    I have updated the DrawGraphics.java.
    When the mouse is over the tires, the dropdown list will show "Tires" selected (I only did for tires and engine).
    I will start to do something after clicking "Go" button.

--- BlackBox/src/blackbox/DrawGraphics.java
+++ BlackBox/src/blackbox/DrawGraphics.java
@@ -1,25 +1,119 @@
 package blackbox;
 
 import javax.swing.*;
-
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class DrawGraphics extends JPanel{
-	
+public class DrawGraphics extends JPanel {
+
+	// private Color color = Color.WHITE;
+	private JLabel xLabel, yLabel, lblPart;
+	private MouseEventAdapterA meaA;
+	private JComboBox selection;
+	private JButton go;
+	private String xStr, yStr;
+	private int x, y;
+
 	DrawGraphics() {
 		setBackground(Color.WHITE);
 		setPreferredSize(new Dimension(620, 400));
-	} 
-	 
-     @Override
-     public void paintComponent(Graphics g) {
-         super.paintComponent(g);   
+		setLayout(null);
+
+		xLabel = new JLabel("");
+		xLabel.setBounds(10, 10, 150, 150);
+		yLabel = new JLabel("");
+		yLabel.setBounds(10, 30, 150, 150);
+		lblPart = new JLabel("");
+		lblPart.setBounds(10, 50, 150, 150);
+
+		selection = new JComboBox();
+		selection.setModel(new DefaultComboBoxModel(new String[] { "Choose the part", "Engine", "Tires" }));
+		selection.setBounds(497, 35, 123, 23);
+
+		go = new JButton("Go");
+		go.setBounds(557, 65, 55, 20);
+		
+		add(xLabel);
+		add(yLabel);
+		add(lblPart);
+		add(selection);
+		add(go);
+
+		addMouseMotionListener(new MouseEventAdapterA());
+		addMouseListener(new MouseEventAdapterA());
+	}
+
+	class MouseEventAdapterA extends MouseAdapter {
+		public void mouseMoved(MouseEvent e) {
+
+			x = e.getX();
+			y = e.getY();
+
+			xStr = new String("<html><h2><font color=blue size=4>" + "X: ");
+			xStr = xStr + x + "</font></h2></html>";
+
+			yStr = new String("<html><h2><font color=green size=4>" + "Y: ");
+			yStr = yStr + y + "</font></h2></html>";
+
+			xLabel.setText(xStr);
+			yLabel.setText(yStr);
+
+			if ((x >= 224 && x <= 305) && (y >= 282 && y <= 367) || (x >= 477 && x <= 537) && (y >= 179 && y <= 265)) {
+				lblPart.setText("Tires");
+			
+			} else if ((x >= 257 && x <= 303) && (y >= 211 && y <= 248)) {
+				lblPart.setText("Engine");
+			}
+
+			else {
+				lblPart.setText("Choose the part");
+			}
+
+			repaint();
+
+		}
+		
+		public void mouseClicked(MouseEvent e) {
+			String text = new String(lblPart.getText());
+			
+			if ((x >= 224 && x <= 305) && (y >= 282 && y <= 367) || (x >= 477 && x <= 537) && (y >= 179 && y <= 265)) {
+				selection.setSelectedItem(text);
+
+			} else if ((x >= 257 && x <= 303) && (y >= 211 && y <= 248)) {
+				selection.setSelectedItem(text);
+
+			} else {
+				selection.setSelectedItem(text);
+
+			}
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
 		Image background = new ImageIcon("image/background.jpg").getImage();
 		g.drawImage(background, 50, 10, this);
-	     
-    }
+
+		g.setColor(Color.ORANGE);
+		g.fillOval(x, y, 25, 25);
+
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setColor(Color.YELLOW);
+		g2d.setFont(new Font("", Font.BOLD, 13));
+		g2d.drawString(lblPart.getText(), x, y);
+
+	}
+
 }
