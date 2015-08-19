77e9365c84333cdc0698a0fdd442bafa55c06792
YuyuXiao <xxiao@scu.edu>
2015-08-18 22:23:31 -0700

    Diagnose for Engine after "Go"

---
+++ BlackBox/src/blackbox/EngineCard.java
@@ -0,0 +1,83 @@
+package blackbox;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class EngineCard extends JPanel {
+	
+	private BlackBoxTester bt;
+	private Car myCar = bt.thisCar.getCar();
+	private RealTimeMonitor rtm;
+	private Sensors s = rtm.sensor.getSensor();
+	
+	private JLabel pagename, lblType;
+	private JLabel type, text1, text2;
+	
+	public EngineCard() {
+		super();
+		setLayout(null);
+		
+		pagename = new JLabel("Engine");
+		pagename.setBounds(290, 11, 46, 14);
+		add(pagename);
+		
+		lblType = new JLabel("Type");
+		lblType.setBounds(24, 81, 88, 14);
+		add(lblType);
+		
+		JLabel type = new JLabel();
+		type.setBounds(150, 81, 180, 14);
+		type.setText(myCar.sysEngine.getType());
+		add(type);
+	
+		text1 = new JLabel(); 
+		text1.setBounds(380, 150, 200, 100);
+		add(text1);
+		
+		text2 = new JLabel(); 
+		text2.setBounds(380, 170, 200, 100);
+		add(text2);
+		
+		JButton td = new JButton("test");
+		td.setBounds(380, 10, 100, 24);
+		td.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Thread.sleep(1500);
+					diagnoseEngine();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		add(td);
+	}
+	
+	public void diagnoseEngine()
+	{
+		DecimalFormat one = new DecimalFormat("#0.0"); // Set digits for decimal numbers
+		text1.setText(one.format(s.getCarRPM()));
+		
+		if(s.getCarRPM() >= 5800) {
+			text2.setText("RPM is too High");
+		}
+		else
+		{
+			text2.setText("Normal");
+		}
+	
+		}
+	
+	public void clearEC()
+	{
+		text1.setText("");
+		text2.setText("");
+	}
+}
---
+++ BlackBox/src/blackbox/TiresCard.java
@@ -0,0 +1,127 @@
+package blackbox;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TiresCard extends JPanel {
+	
+	private BlackBoxTester bt;
+	private Car myCar = bt.thisCar.getCar();
+	private RealTimeMonitor rtm;
+	private Sensors s = rtm.sensor.getSensor();
+	
+	private JLabel pagename, lblBrandname, lblModel, lblServiceDate;
+	private JLabel brandname, model, serviceDate, text;
+	
+	private JLabel tireLF, tireLR, tireRF, tireRR;
+	
+	public TiresCard() {
+		super();
+		setLayout(null);
+		
+		pagename = new JLabel("Tires");
+		pagename.setBounds(290, 11, 46, 14);
+		add(pagename);
+		
+		lblBrandname = new JLabel("Brand");
+		lblBrandname.setBounds(24, 81, 88, 14);
+		add(lblBrandname);
+		
+		lblModel = new JLabel("Model");
+		lblModel.setBounds(24, 121, 88, 14);
+		add(lblModel);
+		
+		lblServiceDate = new JLabel("Last Service Date");
+		lblServiceDate.setBounds(24, 161, 150, 14);
+		add(lblServiceDate);
+		
+		brandname = new JLabel();
+		brandname.setBounds(150, 81, 88, 14);
+		brandname.setText(myCar.sysTires.getBrandName());
+		add(brandname);
+		
+		model = new JLabel();
+		model.setBounds(150, 121, 88, 14);
+		model.setText(myCar.sysTires.getModelNumber());
+		add(model);
+		
+		serviceDate = new JLabel(); 
+		serviceDate.setBounds(150, 161, 88, 14);
+		serviceDate.setText(myCar.sysTires.getServiceDate());
+		add(serviceDate);
+				
+		tireLF = new JLabel(); 
+		tireLF.setBounds(380, 50, 50, 14);
+		add(tireLF);
+
+		tireLR = new JLabel(); 
+		tireLR.setBounds(380, 70, 50, 14);
+		add(tireLR);
+
+		tireRF = new JLabel(); 
+		tireRF.setBounds(380, 90, 50, 14);
+		add(tireRF);
+
+		tireRR = new JLabel(); 
+		tireRR.setBounds(380, 110, 50, 14);
+		add(tireRR);
+		
+		text = new JLabel(); 
+		text.setBounds(380, 150, 200, 100);
+		add(text);
+	
+		JButton td = new JButton("test");
+		td.setBounds(380, 10, 100, 24);
+		td.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Thread.sleep(1500);
+					diagnoseTires();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		add(td);
+	}
+	
+	public void diagnoseTires()
+	{
+		DecimalFormat one = new DecimalFormat("#0.0"); // Set digits for decimal numbers
+		
+		double[] tiresarray = new double[4];
+		tiresarray = s.getTiresPressure();
+		
+		tireLF.setText(one.format(tiresarray[0]));
+		tireLR.setText(one.format(tiresarray[1]));
+		tireRF.setText(one.format(tiresarray[2]));
+		tireRR.setText(one.format(tiresarray[3]));
+		
+		text.setText("Normal");
+		for(int i = 0; i < 4; i++) {
+			if(tiresarray[i] >= 45) {
+				text.setText("High Tire Pressure!");
+			}
+		}
+	}
+	
+	public void clearTC()
+	{
+		tireLF.setText("");
+		tireLR.setText("");
+		tireRF.setText("");
+		tireRR.setText("");
+		text.setText("");
+
+	}
+}
